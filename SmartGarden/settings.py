340b7175b0424a6b27dc93e22f853e4a415e2050
Andrey <superkurlin2013@yandex.ru>
2023-07-06 23:07:35 +0500

    connect front with back

--- SmartGarden/settings.py
+++ SmartGarden/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'SmartGarden.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'front']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'front/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- app/views.py
+++ app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, 'html/index.html')
+
