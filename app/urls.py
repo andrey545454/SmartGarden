d316e054f7c1c0c4c3141377d558a93cd97f1104
Andrey <superkurlin2013@yandex.ru>
2023-07-07 12:33:55 +0500

    basic set of pages

--- app/urls.py
+++ app/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
 
     path('/nature_home', views.nature_home, name='nature_home'),
     path('/nature_outside', views.nature_outside, name='nature_outside'),
+    path('/last_nature_home', views.last_nature_home, name='last_nature_home'),
 
     path('/add_nature', views.add_nature, name='add_nature'),
+    path('/timetable', views.timetable, name='timetable'),
 
     path('/sensors', views.sensors, name='sensors')
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.urls import reverse
 
 def index(request):
     return render(request, 'index.html')
@@ -13,8 +13,16 @@ def nature_outside(request):
     return render(request, 'nature_outside.html')
 
 
+def last_nature_home(request):
+    return render(request, 'last_nature_home.html')
+
+
 def add_nature(request):
     return render(request, 'add_nature.html')
+
+
+def timetable(request):
+    return render(request, 'timetable.html')
 
 
 def sensors(request):
